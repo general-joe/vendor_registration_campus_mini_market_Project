feaf7a252acfbf7ef4e7d3e5d74961c7f3c1a9c6
D-General <kwabijoe274@gmail.com>
2023-11-26 22:55:00 +0000

    Added vendorRouter as a middleware in the server

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 const cors = require("cors");
 
 require("dotenv").config();
-const vendorRoute = require("./routes/vendorRoute");
+
 const PORT = process.env.PORT;
+const vendorRoute = require("./routes/vendorRoute");
 
 app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
+app.use(vendorRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
