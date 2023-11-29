4a6ccfc9f063c0f5866e58aa141463640948d8d6
D-General <kwabijoe274@gmail.com>
2023-11-29 23:12:52 +0000

    Finalized validation on vendor's details

--- routes/vendorRoute.js
+++ routes/vendorRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const vendorController = require("../controllers/vendorController");
+const validatonError = require("../utilities/validationError");
+const validationScheme = require("../utilities/validationScheme");
+const valid = [...validationScheme, validatonError.validateRequestSchema];
 
-router.post("/register", vendorController.signUpVendor);
+router.post("/register", valid, vendorController.signUpVendor);
 router.get("/register/:id", vendorController.getSingleVendor);
 router.patch("/register/:id", vendorController.updateSingleVendor);
 router.post("/login", vendorController.loginVendor);
