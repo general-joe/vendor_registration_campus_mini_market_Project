f1a45f9b338c8948f0f7d4f099f80c4262353577
D-General <kwabijoe274@gmail.com>
2023-12-10 22:21:57 +0000

    Code review and resolved

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -5,29 +5,26 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const signUpVendor = async (req, res) => {
-  let { firstName, lastName, email, password, phoneNumber, business } =
-    req.body;
+  // let { firstName, lastName, email, password, phoneNumber, business } =
+  //   req.body;wrong
+  const { business, ...rest } = req.body;
   try {
     // This will check whether vendor's details already exist in the database
     const existingVendor = await prisma.vendor.findFirst({
-      where: { email },
+      where: { email: rest.email },
     });
     if (existingVendor) {
-      return res.status(409).json({ massage: "Vendor has already registered" });
+      return res.status(409).json({ message: "Vendor has already registered" });
     } else {
       //Hash the password before storing it in the database
-      const hashedPassword = await bcrypt.hash(password, 10);
-      password = hashedPassword;
+      const hashedPassword = await bcrypt.hash(rest.password, 10);
+      rest.password = hashedPassword;
 
       //This line of code is going to create a vendor
 
       const newVendor = await prisma.vendor.create({
         data: {
-          firstName,
-          lastName,
-          email,
-          password,
-          phoneNumber,
+          ...rest,
           business: {
             create: business,
           },
@@ -35,20 +32,21 @@ const signUpVendor = async (req, res) => {
         include: { business: true },
       });
       res.status(201).json({
-        massage: "Vendor successfully registered",
+        message: "Vendor successfully registered",
         vendor: newVendor,
       });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ massage: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 //This code is use to get a single vendor by id
 
 const getSingleVendor = async (req, res) => {
-  const id = req.params.id;
+  //const id = req.params.id;wrong
+  const { id } = req.params;
   try {
     const getSingleVendor = await prisma.vendor.findUnique({
       where: { id },
@@ -57,14 +55,15 @@ const getSingleVendor = async (req, res) => {
     res.status(200).json({ getSingleVendor });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ massage: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 //This code is for updating a vendor
 
 const updateSingleVendor = async (req, res) => {
-  const id = req.params.id;
+  //const id = req.params.id;wrong
+  const { id } = req.params;
   const data = req.body;
   try {
     const updateVendor = await prisma.vendor.update({
@@ -75,7 +74,7 @@ const updateSingleVendor = async (req, res) => {
     res.status(200).json({ updateVendor });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ massage: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
@@ -89,14 +88,14 @@ const loginVendor = async (req, res) => {
       include: { business: true },
     });
     if (!vendor || !(await bcrypt.compare(password, vendor.password))) {
-      return res.status(401).json({ massage: "Invalid credentials" });
+      return res.status(401).json({ message: "Invalid credentials" });
     } else {
       const token = jwt.sign({ userId: vendor.id }, process.env.SECRET_KEY);
       res.json({ token, vendor });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ massage: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
